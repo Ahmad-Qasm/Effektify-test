f276d5286bcd1813b548be4345dda7f81461c5b2
ahmad qassim <amqsm.93@gmail.com>
2023-07-13 15:07:12 +0200

    added style to visulise the metrics

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const root= document.querySelector(".root")
+const metricsContainer = document.querySelector(".metric-container");
 
-function visualiseMetric(metrics){
-    console.log("here we go", metrics);
-    let toBeVisualised = metrics.map(metric => {
-        return `<h3>${metric["key"]}</h3>`
+function visualiseMetric(metrics) {
+  return metrics
+    .map((metric) => {
+      return `<article class="metric">
+          <h3>${metric.key}</h3>
+          <h5>${metric.env}</h5>
+          <p class="description">description: ${metric.description}</p>
+          <p class="values">value: ${metric.values.value}</p>
+        </article>`;
     })
-    console.log(toBeVisualised);
-    return toBeVisualised.join("\n")
+    .join("\n");
 }
-const metrecis = fetch('https://platform-staging.effektify.com/api/v1/inhouse/statistics', {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer 4313BAD127DEB8C8C2494181BA547`
-    }}).then(res => res.json()).then(data => {
-        console.log(data);
-        const str = visualiseMetric(data["metrics"])
-        root.innerHTML = str
-        console.log("elems ---->",str);
-    })
-console.log(metrecis);
 
-
-window.addEventListener("DOMContentLoaded", ()=>{
-    root.innerHTML = `<h4>sdfdsfdsgdh<h4/>`
-    console.log("hello it is me");
+const url = "https://platform-staging.effektify.com/api/v1/inhouse/statistics";
+const authKey = "4313BAD127DEB8C8C2494181BA547";
+const metrecis = fetch(url, {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${authKey}`,
+  },
 })
+  .then((res) => res.json())
+  .then((data) => {
+    const metrics = data["metrics"];
+    const str = visualiseMetric(metrics);
+    metricsContainer.innerHTML = str;
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
